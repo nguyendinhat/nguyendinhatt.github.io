4ba2273718cf61c0ccbb1e2ea3eeedeb462474f0
Nguyễn Đình Ất <nguyendinhatt@gmail.com>
2017-08-18 00:30:37 +0700

    success

--- main.js
+++ main.js
@@ -23,7 +23,6 @@ function openStream() {
     const config = { audio: true, video: true };
     return navigator.mediaDevices.getUserMedia(config);
 }
-
 function playStream(idVideoTag, stream) {
     const video = document.getElementById(idVideoTag);
     video.srcObject = stream;
